d4c20c780200e3ba7025d4e275eb124ccf771097
nikhil.wadatkar4693@gmail.com <Ankur@4693>
2024-07-30 09:06:59 +0530

    project structure added

---
+++ school-dashboard/src/components/Dashboard.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function Dashboard() {
+    return (
+        <div className='container-fluid'>
+            <h1>Dashboard</h1>
+            <div>
+                <nav className="nav nav-pills flex-column flex-sm-row">
+                    <a className="flex-sm-fill text-sm-center nav-link active" aria-current="page" href="#">Active</a>
+                    <a className="flex-sm-fill text-sm-center nav-link" href="#">Longer nav link</a>
+                    <a className="flex-sm-fill text-sm-center nav-link" href="#">Link</a>
+                    <a className="flex-sm-fill text-sm-center nav-link" href="#">Link</a>
+                    <a className="flex-sm-fill text-sm-center nav-link" href="#">Link</a>
+                    <a className="flex-sm-fill text-sm-center nav-link" href="#">Link</a>
+                    <a className="flex-sm-fill text-sm-center nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                </nav>
+            </div>
+
+            <div className='row'>
+                <div className='col' style={{ backgroundColor: "gray" }}><p>Menus</p></div>
+                <div className='col' style={{ backgroundColor: "green" }}><p>main section</p></div>
+            </div>
+        </div>
+    )
+}
+
+export default Dashboard
