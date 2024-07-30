95f662005dd29b71a1f4c397bd1d91aaa7dd84e1
nikhil.wadatkar4693@gmail.com <artofprogramming2@gmail.com>
2024-07-31 00:50:33 +0530

    added form

--- school-dashboard/src/components/Dashboard.js
+++ school-dashboard/src/components/Dashboard.js
@@ -1,26 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Dashboard() {
+    const [itemList,setItems]=useState(["Class","Students","Subject","Class Routine","Parent","Student Section"]);
     return (
-        <div className='container'>
+        <div className='container-fluid'>
             <h1>Dashboard</h1>
-            {/* <div>
-                <nav className="nav nav-pills flex-column flex-sm-row">
-                    <a className="flex-sm-fill text-sm-center nav-link active" aria-current="page" href="#">Active</a>
-                    <a className="flex-sm-fill text-sm-center nav-link" href="#">Longer nav link</a>
-                    <a className="flex-sm-fill text-sm-center nav-link" href="#">Link</a>
-                    <a className="flex-sm-fill text-sm-center nav-link" href="#">Link</a>
-                    <a className="flex-sm-fill text-sm-center nav-link" href="#">Link</a>
-                    <a className="flex-sm-fill text-sm-center nav-link" href="#">Link</a>
-                    <a className="flex-sm-fill text-sm-center nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                </nav>
-            </div> */}
-<div className='container'>
-            <div className='row'>
-                <div className='col-4' style={{ backgroundColor: "gray" }}><p>Menus</p></div>
-                <div className='col-8' style={{ backgroundColor: "green" }}><p>main section</p></div>
+            <div className='container-fluid'>
+                <div className='row'>
+                    <div className='col-2' >
+                        <div>
+                            <ul className="list-group">
+                                {
+                                   itemList.map((item, index) => (
+                                    <li className="list-group-item">{item}</li>
+                                   ))
+                                }
+                                <li className="list-group-item">An item</li>
+                                <li className="list-group-item">A second item</li>
+                                <li className="list-group-item">A third item</li>
+                                <li className="list-group-item">A fourth item</li>
+                                <li className="list-group-item">And a fifth one</li>
+                            </ul>
+                        </div>
+
+                    </div>
+                    <div className='col-10' style={{ backgroundColor: "green" }}><p>main section</p></div>
+                </div>
             </div>
-            </div>
+            <div className="footer fixed-bottom">
+    2 days ago
+  </div>
         </div>
     )
 }
