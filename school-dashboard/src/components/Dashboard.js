49b7b9bd5d804a79aec27255c1d4e2283f99556c
nikhil.wadatkar4693@gmail.com <Ankur@4693>
2024-07-30 23:49:30 +0530

    project structure added

--- school-dashboard/src/components/Dashboard.js
+++ school-dashboard/src/components/Dashboard.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Dashboard() {
     return (
-        <div className='container-fluid'>
+        <div className='container'>
             <h1>Dashboard</h1>
-            <div>
+            {/* <div>
                 <nav className="nav nav-pills flex-column flex-sm-row">
                     <a className="flex-sm-fill text-sm-center nav-link active" aria-current="page" href="#">Active</a>
                     <a className="flex-sm-fill text-sm-center nav-link" href="#">Longer nav link</a>
@@ -14,11 +14,12 @@ function Dashboard() {
                     <a className="flex-sm-fill text-sm-center nav-link" href="#">Link</a>
                     <a className="flex-sm-fill text-sm-center nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
                 </nav>
-            </div>
-
+            </div> */}
+<div className='container'>
             <div className='row'>
-                <div className='col' style={{ backgroundColor: "gray" }}><p>Menus</p></div>
-                <div className='col' style={{ backgroundColor: "green" }}><p>main section</p></div>
+                <div className='col-4' style={{ backgroundColor: "gray" }}><p>Menus</p></div>
+                <div className='col-8' style={{ backgroundColor: "green" }}><p>main section</p></div>
+            </div>
             </div>
         </div>
     )
