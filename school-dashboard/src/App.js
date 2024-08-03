00b879a5fafb3b26e7536f0e5036c7cd1e785c47
nikhil.wadatkar4693@gmail.com <Ankur@4693>
2024-08-03 21:23:33 +0530

    files addeed

--- school-dashboard/src/App.js
+++ school-dashboard/src/App.js
@@ -1,58 +1,77 @@
-import React, { createContext } from 'react';
-import './App.css';
+import React, { createContext } from "react";
+import "./App.css";
 import { Routes, Route, useNavigate } from "react-router-dom";
-import CreateUser from './components/CreateUser';
-import Dashboard from './components/Dashboard';
-import { useState } from 'react';
-import { routes } from './Constants';
-import TeachersList from './components/TeachersList';
-import { Modal } from 'bootstrap';
-import ModalDemo from './components/ModalDemo';
-import Navbar from './components/Navbar';
-import HorizontalNavbar from './components/HorizontalNavbar';
-import { MyContext } from './components/MyContext';
-import AdminTeachersExport from './components/Admin/AdminTeachersExport';
+import CreateUser from "./components/CreateUser";
+import Dashboard from "./components/Dashboard";
+import { useState } from "react";
+import { routes } from "./Constants";
+import TeachersList from "./components/TeachersList";
+import { Modal } from "bootstrap";
+import ModalDemo from "./components/ModalDemo";
+import Navbar from "./components/Navbar";
+import HorizontalNavbar from "./components/HorizontalNavbar";
+import { MyContext } from "./components/MyContext";
+import AdminTeachersExport from "./components/Admin/AdminTeachersExport";
 
 function App() {
   const [text, setText] = useState("Admin");
+  const [alert, setAlert] = useState(false);
+  const [alertMessage, setAlertMessage] = useState("");
+  const [alertTitle, setAlertTitle] = useState("");
+  const [messageType, setMessageType] = useState("alert-success");
 
   return (
-    <div >
-      <MyContext.Provider value={{ text, setText }}>
-        <h1 style={{ backgroundColor: "#79d789", color: "white" }}>Dashboard</h1>
+    <div>
+      <MyContext.Provider
+        value={{
+          text,
+          setText,
+          alert,
+          setAlert,
+          alertMessage,
+          setAlertMessage,
+          messageType,
+          setMessageType,
+          alertTitle,
+          setAlertTitle,
+        }}
+      >
+        <h1 style={{ backgroundColor: "#79d789", color: "white" }}>
+          Dashboard
+        </h1>
         <HorizontalNavbar></HorizontalNavbar>
-        <div >
-          <div className='row'>
-            <div className='col-2' >
-              {/* <Navbar></Navbar> */}
 
-            </div>
-            <div className='col-8' ><p>main section</p>
+        <div>
+          <div className="row">
+            <div className="col-2">{/* <Navbar></Navbar> */}</div>
+            <div className="col-8">
+              <p>main section</p>
 
               <Routes>
                 <Route path="/create" element={<CreateUser />} />
                 <Route path="/modal" element={<ModalDemo />} />
-                <Route path="/teacherList" element={<TeachersList></TeachersList>} />
-                <Route path='/adminTeacherExcel' element={<AdminTeachersExport/>}/>
+                <Route
+                  path="/teacherList"
+                  element={<TeachersList></TeachersList>}
+                />
+                <Route
+                  path="/adminTeacherExcel"
+                  element={<AdminTeachersExport />}
+                />
                 {/* <Route index element={<Home />} />
                   <Route path="blogs" element={<Blogs />} />
                   <Route path="contact" element={<Contact />} />
                   <Route path="*" element={<NoPage />} /> */}
                 <Route path="/dash" element={<Dashboard />} />
               </Routes>
-
-
-
             </div>
-            <div className='col-2'>Notifications</div>
+            <div className="col-2">Notifications</div>
           </div>
         </div>
-        <div className="footer fixed-bottom">
-          2 days ago
-        </div>
+        <div className="footer fixed-bottom">2 days ago</div>
       </MyContext.Provider>
     </div>
-  )
+  );
 }
 
 export default App;
--- school-dashboard/src/components/CreateUser.js
+++ school-dashboard/src/components/CreateUser.js
@@ -1,84 +1,230 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from "react";
+import AlertMessage from "./AlertMessage";
+import { MyContext } from "./MyContext";
+import {
+  onlyAlphabetValidator,
+  phoneValiddator,
+  zipCodeValidator,
+} from "../util/Validator";
 
 function CreateUser() {
-    const [userTypeList, setUserTypeList] = useState(["Student", "Teacher", "Peon", "Parent"]);
-    const [stateList, setStateList] = useState(["MH", "AP", "TG"]);
-    const [userDetails, setUserDetails] = useState({
-        firstname: "",
-        lastname: "",
-        age: "",
-        contact: "",
-        userType: "",
-        email: "",
-        city: "",
-        zipCode: "",
-        password: "",
-        address:""
-    });
-    const handleChange = (key, val) => {
-        console.log("key:", key, "   val:", val);
-        setUserDetails({ ...userDetails, [key]: val })
+  const {
+    text,
+    setText,
+    alert,
+    setAlert,
+    alertMessage,
+    setAlertMessage,
+    messageType,
+    setMessageType,
+    alertTitle,
+    setAlertTitle,
+  } = useContext(MyContext);
+  const [userTypeList, setUserTypeList] = useState([
+    "Student",
+    "Teacher",
+    "Peon",
+    "Parent",
+  ]);
+  const [stateList, setStateList] = useState(["MH", "AP", "TG"]);
+  const [userDetails, setUserDetails] = useState({
+    firstname: "",
+    lastname: "",
+    age: "",
+    contact: "",
+    userType: "",
+    email: "",
+    city: "",
+    zipCode: "",
+    password: "",
+    address: "",
+  });
+  const [showAlert, setShowAlert] = useState(false);
+  const handleChange = (key, val) => {
+    console.log("key:", key, "   val:", val);
+    setUserDetails({ ...userDetails, [key]: val });
+  };
+  const submitDetails = (e) => {
+    e.preventDefault();
+    console.log("Details::", userDetails);
+    let isValid = validateRequest(userDetails);
+    if (isValid.error === false) {
+      setAlert(true);
+      setAlertTitle("Sorry BHaiii!!");
+      setAlertMessage(isValid.message);
+      setMessageType("alert-danger");
+      setShowAlert(true);
+    } else {
+      setAlert(false);
+      setShowAlert(false);
     }
-    const submitDetails = (e) => {
-        e.preventDefault();
-        console.log("Details::", userDetails);
-    }
-    return (
-        <div>
-            <h1>User</h1>
-            <form className="row g-3">
+  };
 
-                <div className="col-sm-5">
-                    <label for="inputAddress" className="form-label">First Name</label>
-                    <input type="text" className="form-control" id="inputAddress" placeholder="Name" onChange={(e) => handleChange("firstname", e.target.value)} />
-                </div>
-                <div className="col-sm-5">
-                    <label for="inputAddress" className="form-label">Last Name</label>
-                    <input type="text" className="form-control" id="inputAddress" placeholder="Last Name" onChange={(e) => handleChange("lastname", e.target.value)} />
-                </div>
-                <div className="col-sm-2">
-                    <label for="inputAddress" className="form-label">Age</label>
-                    <input type="text" className="form-control" id="inputAddress" onChange={(e) => handleChange("age", e.target.value)} />
-                </div>
-                <div className="col-md-6">
-                    <label for="inputEmail4" className="form-label">Email</label>
-                    <input type="email" className="form-control" id="inputEmail4"onChange={(e) => handleChange("email", e.target.value)} />
-                </div>
-                <div className="col-md-6">
-                    <label for="inputPassword4" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="inputPassword4" onChange={(e) => handleChange("password", e.target.value)} />
-                </div>
-                <div className="col-6">
-                    <label for="inputAddress2" className="form-label">Address 2</label>
-                    <input type="text" className="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor"
-                    onChange={(e) => handleChange("address", e.target.value)} />
-                </div>
-                <div className="col-6">
-                    <label for="inputAddress2" className="form-label">Contact</label>
-                    <input type="text" className="form-control" id="inputAddress2" placeholder="123465890"
-                    onChange={(e) => handleChange("contact", e.target.value)} />
-                </div>
-                <div className="col-md-6">
-                    <label for="inputCity" className="form-label">City</label>
-                    <input type="text" className="form-control" id="inputCity" onChange={(e) => handleChange("city", e.target.value)} />
-                </div>
-                <div className="col-md-4">
-                    <label for="inputState" className="form-label">Select Type:</label>
-                    <select id="inputState" className="form-select" onChange={(e) => handleChange("userType", e.target.value)}>
-                        <option selected>Choose...</option>
-                        {
-                            userTypeList.map((item, index) => (
-                                <option key={index} value={item}>{item}</option>
-                            ))
-                        }
-                    </select>
-                </div>
-                <div className="col-md-2">
-                    <label for="inputZip" className="form-label">Zip</label>
-                    <input type="text" className="form-control" id="inputZip" onChange={(e) => handleChange("zipCode", e.target.value)} />
-                </div>
+  const validateRequest = (reqDetails) => {
+    let errorDetails = {
+      error: false,
+      message: "",
+    };
+    let message = "";
+    if (
+      reqDetails.firstname === "" ||
+      !onlyAlphabetValidator(reqDetails.firstname)
+    )
+      message = message + " First Name ";
+    if (!onlyAlphabetValidator(reqDetails.lastname))
+      message = message + " Last Name ";
+    if (!zipCodeValidator(reqDetails.zipCode)) message = message + " Zip Code ";
+    if (!phoneValiddator(reqDetails.contact)) message = message + " Zip Code ";
+    if (!phoneValiddator(reqDetails.age)) message = message + " Age ";
+    if (!onlyAlphabetValidator(reqDetails.address))
+      message = message + " Address ";
+    if (!onlyAlphabetValidator(reqDetails.city)) message = message + " City ";
 
-                {/* <div className='col-sm-1'>
+    if (message === "")
+      return {
+        error: true,
+        message: "",
+      };
+    else
+      return {
+        error: false,
+        message: message,
+      };
+  };
+
+  return (
+    <div>
+      <h1>User</h1>
+      {showAlert ? (
+        <>
+          <AlertMessage></AlertMessage>
+        </>
+      ) : (
+        ""
+      )}
+      <form className="row g-3">
+        <div className="col-sm-5">
+          <label for="inputAddress" className="form-label">
+            First Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="inputAddress"
+            placeholder="Name"
+            onChange={(e) => handleChange("firstname", e.target.value)}
+          />
+        </div>
+        <div className="col-sm-5">
+          <label for="inputAddress" className="form-label">
+            Last Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="inputAddress"
+            placeholder="Last Name"
+            onChange={(e) => handleChange("lastname", e.target.value)}
+          />
+        </div>
+        <div className="col-sm-2">
+          <label for="inputAddress" className="form-label">
+            Age
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="inputAddress"
+            onChange={(e) => handleChange("age", e.target.value)}
+          />
+        </div>
+        <div className="col-md-6">
+          <label for="inputEmail4" className="form-label">
+            Email
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="inputEmail4"
+            onChange={(e) => handleChange("email", e.target.value)}
+          />
+        </div>
+        <div className="col-md-6">
+          <label for="inputPassword4" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="inputPassword4"
+            onChange={(e) => handleChange("password", e.target.value)}
+          />
+        </div>
+        <div className="col-6">
+          <label for="inputAddress2" className="form-label">
+            Address 2
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="inputAddress2"
+            placeholder="Apartment, studio, or floor"
+            onChange={(e) => handleChange("address", e.target.value)}
+          />
+        </div>
+        <div className="col-6">
+          <label for="inputAddress2" className="form-label">
+            Contact
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="inputAddress2"
+            placeholder="123465890"
+            onChange={(e) => handleChange("contact", e.target.value)}
+          />
+        </div>
+        <div className="col-md-6">
+          <label for="inputCity" className="form-label">
+            City
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="inputCity"
+            onChange={(e) => handleChange("city", e.target.value)}
+          />
+        </div>
+        <div className="col-md-4">
+          <label for="inputState" className="form-label">
+            Select Type:
+          </label>
+          <select
+            id="inputState"
+            className="form-select"
+            onChange={(e) => handleChange("userType", e.target.value)}
+          >
+            <option selected>Choose...</option>
+            {userTypeList.map((item, index) => (
+              <option key={index} value={item}>
+                {item}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="col-md-2">
+          <label for="inputZip" className="form-label">
+            Zip
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="inputZip"
+            onChange={(e) => handleChange("zipCode", e.target.value)}
+          />
+        </div>
+
+        {/* <div className='col-sm-1'>
                     {
                         userTypeList.map((item, index) => (
                             <div class="form-check">
@@ -93,14 +239,44 @@ function CreateUser() {
                     }
                 </div> */}
 
-                <div className="col-12">
-                    <button type="submit" className="btn btn-primary" onClick={(e) => submitDetails(e)}>Sign in</button>
-                </div>
-            </form>
-            <br></br><br></br><br></br><br></br><br></br><br></br><br></br>  <br></br><br></br><br></br><br></br><br></br><br></br><br></br>  <br></br><br></br><br></br><br></br><br></br><br></br><br></br>  <br></br><br></br><br></br><br></br><br></br><br></br><br></br>
-            <p>ddd</p>
+        <div className="col-12">
+          <button
+            type="submit"
+            className="btn btn-primary"
+            onClick={(e) => submitDetails(e)}
+          >
+            Sign in
+          </button>
         </div>
-    )
+      </form>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br> <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br> <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br> <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <p>ddd</p>
+    </div>
+  );
 }
 
-export default CreateUser
+export default CreateUser;
