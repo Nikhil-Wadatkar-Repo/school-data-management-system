15e77c87489267674fda9411ef2d4754482d8dec
nikhil.wadatkar4693@gmail.com <artofprogramming2@gmail.com>
2024-10-16 06:58:33 +0530

    added

---
+++ attendance-leave-management-tracker/src/test/java/com/nt/attendance_leave_management_tracker/AttendanceLeaveManagementTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nt.attendance_leave_management_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceLeaveManagementTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
